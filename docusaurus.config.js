2672b5f994761877ffc28eec7be6d15874204115
Aurelian Spodarec <aurelian.spod@gmail.com>
2022-04-06 06:54:32 +0100

    Changing base to root

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Lua Documentation',
+  tagline: 'Lua Docs',
   url: 'https://luavel.com',
-  baseUrl: '/docs/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'AurelianSpodarec', // Usually your GitHub org/user name.
+  projectName: 'lua-documentation', // Usually your repo name.
 
   presets: [
     [
