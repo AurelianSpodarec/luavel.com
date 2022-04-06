44008e92c663f6c0174f03b64f43bc82c229c7f0
Aurelian Spodarec <aurelian.spod@gmail.com>
2022-04-06 07:03:29 +0100

    Changed some docsaurus settings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Lua Documentation',
   tagline: 'Lua Docs',
   url: 'https://luavel.com',
-  baseUrl: '/',
+  baseUrl: '/docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
