a2e751a2fd369eb7cca01a572027fa03fdfce6ef
Aurelian Spodarec <aurelian.spod@gmail.com>
2022-04-06 06:48:29 +0100

    Changed base url again

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://luavel.com',
-  baseUrl: '/docs',
+  baseUrl: '/docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
